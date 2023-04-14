01949817ceddec516ac43c61414f4c3f3ff9d6bd
Jay M <jaymansmann@yahoo.com>
2023-04-14 16:57:53 -0400

    Added real array of movies for future days

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import movies from "./data/movies";
 
 function App() {
   const movieOfTheDay = movies[0]; // TODO increment to the next day magically
+  const allMoviesEver = ['Avatar', 'John Wick', 'Jurassic Park']; // TODO pull from API
+  const guessTracker = []; // TODO use state
+
 
   const onGuess = (e) => {
     e.preventDefault();
     const formData = Object.fromEntries(new FormData(e.target));
     console.table(formData);
-    if(formData.guess.toLowerCase() === movieOfTheDay.title.toLowerCase()) {
+    const isRight = formData.guess.toLowerCase() === movieOfTheDay.title.toLowerCase()
+    guessTracker.push({...formData, isRight}); // TODO eventually do statistics (how many guesses to get right on average, etc)
+    if(isRight) {
+      // TODO display failed and successful guesses
       alert("yay");
     }
+    else {
+      alert("Oh no")
+    }
   }
+
+  const allMoviesEverOptions = allMoviesEver.map(movie => (<option value={movie} key={movie}/>))
 
   const gifs = movieOfTheDay.gifs.map(gif => (<img
     src={gif.src}
@@ -34,8 +45,7 @@ function App() {
           <form onSubmit={onGuess}>
             <input list="movies" name="guess" />
             <datalist id="movies">
-              <option value="John Wick" />
-              <option value="Jurassic Park" />
+              {allMoviesEverOptions}
             </datalist>
             <input type="submit" />
           </form>
--- src/data/movies.js
+++ src/data/movies.js
@@ -12,6 +12,509 @@ const movies = [
       },
     ],
   },
+  {
+    title: "Lethal Weapon",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "John Carter",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Bridget Jones's Diary",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Scary Movie",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Scooby-Doo",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Tomb Raider",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Nutty Professor",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Bad Boys II",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Silence of the Lambs",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Grown Ups",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Neighbors",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Firm",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "10,000 BC",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "American Gangster",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Avatar",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Titanic",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Lion King",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Black Panther",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Frozen",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Beauty and the Beast",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Aquaman",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Skyfall",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Jurassic Park",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Dark Knight Rises",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Joker",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Alice in Wonderland",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Dark Knight",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Harry Potter",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Jungle Book",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Doctor Strange",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Lord of the Rings",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Spider-Man",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Star Wars",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "The Hunger Games",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Guardians of the Galaxy",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
+
+  {
+    title: "Wonder Woman",
+    gifs: [
+      {
+        src: "",
+        alt: "",
+      },
+      {
+        src: "",
+        alt: "",
+      },
+    ],
+  },
 ];
 
 module.exports = movies;
